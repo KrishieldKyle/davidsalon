093db56076853f389c9f1a4bb4f5655826247477
krishield kyle santoyo <krishieldkyle@gmail.com>
2018-11-09 10:50:36 +0800

    changed port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require("path");
 
 var app = express();
 
-const server_port = process.env.PORT || this.SERVER_PORT || 5510;
+const server_port = process.env.PORT || this.SERVER_PORT || 2400;
 const env = process.env.NODE_ENV || "development";
 
 //View Engine
@@ -18,34 +18,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //Set Static Path
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.render("index");
 });
 
-app.get("/home", function (req, res) {
+app.get("/home", function(req, res) {
   res.render("home/home");
 });
 
-app.get("/my-campaign", function (req, res) {
+app.get("/my-campaign", function(req, res) {
   res.render("campaign/my-campaign");
 });
 
-app.get("/upload-campaign", function (req, res) {
+app.get("/upload-campaign", function(req, res) {
   res.render("campaign/upload-campaign");
 });
 
-app.get("/manage-campaign", function (req, res) {
+app.get("/manage-campaign", function(req, res) {
   res.render("campaign/manage-campaign");
 });
 
-app.get("/update-campaign", function (req, res) {
+app.get("/update-campaign", function(req, res) {
   res.render("campaign/update-campaign-status");
 });
 
-app.get("/monitoring", function (req, res) {
+app.get("/monitoring", function(req, res) {
   res.render("monitoring/monitoring");
 });
 
-app.listen(server_port, function () {
+app.listen(server_port, function() {
   console.log("Server started on port " + server_port + ".");
 });
